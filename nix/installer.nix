49aa6fb1c0fc0cc84ed0dbe08666c4a2d79eba02
user <email>
2020-05-17 19:14:05 +0200

    started readme, autologin as root in installer for convenience

--- nix/installer.nix
+++ nix/installer.nix
@@ -1,5 +1,5 @@
 {pkgs, lib, ...}: {
   #nixpkgs.config.allowUnfree = true;
   imports = [ (import ./kernel.nix {}) ];
-  services.mingetty.autologinUser = lib.mkForce "nixos";
+  services.mingetty.autologinUser = lib.mkForce "root";
   }
